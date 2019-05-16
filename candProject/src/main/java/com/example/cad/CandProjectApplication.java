f4908930046c7753f5c3f97590d299d55d16efde
minch <minch@kico.co.jp>
2019-05-16 13:54:46 +0900

    testdf

---
+++ candProject/src/main/java/com/example/cad/CandProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.cad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandProjectApplication.class, args);
+	}
+
+}
